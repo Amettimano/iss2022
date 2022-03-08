7310a7ff32348e18af5a05206571781aff987778
Amettimano <mett.alessandro@gmail.com>
2022-03-08 16:41:40 +0100

    aggiunta di domain

---
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/IDistance.java
@@ -0,0 +1,8 @@
+package it.unibo.radarSystem22.domain;
+
+public interface IDistance {
+	
+	public int getVal();
+	public String toString();
+
+}
---
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/IRadarDisplay.java
@@ -0,0 +1,7 @@
+package it.unibo.radarSystem22.domain;
+
+public interface IRadarDisplay {
+	
+	public void update(String d, String a);
+	
+}
